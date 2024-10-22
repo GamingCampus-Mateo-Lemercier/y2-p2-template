11537269fe657f3b4bea1c42f5ca11d798f31737
Mateo Lemercier <mlemercier@gaming.tech>
2024-10-22 14:31:07 +0200

    [REF] main tests

--- exercice-1bis/main.cpp
+++ exercice-1bis/main.cpp
@@ -11,15 +11,18 @@ int main()
     inventory.AddItem( new Item("Iron Pickaxe", 23.62f) );
     inventory.AddItem( new Item("Diamond Pickaxe", 10.62f) );
     inventory.AddItem( new Item("Netherite Pickaxe", 262.16f) );
-    std::cout << inventory << std::endl;
+    std::cout << "Base " << inventory << std::endl;
     inventory.SortByName();
-    std::cout << inventory << std::endl;
+    std::cout << "Sorted by Name " << inventory << std::endl;
     inventory.SortByNameReverse();
-    std::cout << inventory << std::endl;
+    std::cout << "Sorted by Name Reversed " << inventory << std::endl;
     inventory.SortByWeight();
-    std::cout << inventory << std::endl;
+    std::cout << "Sorted by Weight " << inventory << std::endl;
     inventory.SortByWeightReverse();
-    std::cout << inventory << std::endl;
-    inventory.PrintItem( "Wooden Pickaxe" );
+    std::cout << "Sorted by Weight Reversed " << inventory << std::endl;
+    inventory.FilterItems( []( const Item* const pItem ){ return pItem->GetWeight() > 10.0f; } );
+    std::cout << "Filtered by Weight above 10kg " << inventory << std::endl;
+    inventory.ClearFilters();
+    std::cout << "Filters removed " << inventory << std::endl;
     return 0;
 }
